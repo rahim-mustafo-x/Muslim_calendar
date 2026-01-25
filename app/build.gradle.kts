29d022079242a49f6111d5ff76c71fce210fab9c
Developer-Mustafo <mustaforahimov30@gmail.com>
2026-01-25 20:16:57 +0500

    finishing new 1.5 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uz.coder.muslimcalendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
